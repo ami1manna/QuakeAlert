0e4809a160f01ba155a1e61353fc9cc6a1a14290
ami1manna <amitmannasm@gmail.com>
2024-07-18 20:45:54 +0530

    added Login Signup Feature

---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+// import axios from 'axios';
+// const getUserByEmail = async (email) => {
+//     try{
+//         const response = await axios.get(`http://localhost:5000/auth/user/${email}`);
+//         return response.data.user;//use name 
+//     }
+//     catch(error){
+//             console.error('Error fetching user data : ',error);
+//             return null;
+//     }
+// }
+// export default getUserByEmail; 
